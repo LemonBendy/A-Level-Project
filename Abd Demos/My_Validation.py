49c38fa0e2cb7044fe042f873f8c249f5c95d478
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2023-04-28 14:17:22 +0100

    Validation

--- Abd Demos/My_Validation.py
+++ Abd Demos/My_Validation.py
@@ -1,3 +1,5 @@
+import re
+
 def is_valid_length(text, length, s=0): # 0 for equal, 1 for more than, 2 for less than
     match s:
         case 0:  # default
@@ -18,4 +20,19 @@ def is_valid_range(text, max, min, s=0): # 0 for equal, 1 for more than, 2 for l
             return int(len(text)) < int(min)
 
 def is_valid_email(email):
-    return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+    return bool(re.search(r"^[\w.+\-]+@[\w]+\.[a-z]{2,3}$", email))
+
+def is_valid_integer(text):
+    return bool(re.search(r"^[0-9]+$", text))#
+
+def is_valid_float(text):
+    return bool(re.search(r"^[0-9]+\.[0-9]+$", text))
+
+def is_valid_string(text):
+    return bool(re.search(r"^[a-zA-Z0-9]+$", text))
+
+def is_valid_date(text):
+    return bool(re.search(r"/^(?:31([/\-.])(?:0?[13578]|1[02])\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?"
+                          r":1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|"
+                          r"[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:"
+                          r"0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/gm", text))
