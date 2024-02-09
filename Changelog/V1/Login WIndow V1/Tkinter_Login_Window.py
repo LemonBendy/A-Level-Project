6f87b6a06357bb0cdf316f9bfa5473cc37b684de
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2024-02-09 13:26:49 +0000

    .

--- Changelog/V1/Login WIndow V1/Tkinter_Login_Window.py
+++ Changelog/V1/Login WIndow V1/Tkinter_Login_Window.py
@@ -48,11 +48,11 @@ class user_database:
             print(e)
             return False
 
-    def insert_data(username, password, admin_status=False, face_num=1):
+    def insert_data(username, password, admin_status=False):
         try:
             conn = sq.connect("Changelog\V1\Login WIndow V1/login.db") # Connect to the database
             c = conn.cursor() # Create a cursor
-            c.execute("INSERT INTO login VALUES (?,?,?,?)", (username, password, admin_status, face_num))
+            c.execute("INSERT INTO login VALUES (?,?,?,?)", (username, password, admin_status)
            #^ inserts specified data from the parameters into the database
             conn.commit() # commits the changes
             conn.close() # closes the connection
